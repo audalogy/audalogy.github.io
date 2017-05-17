d8f33e1e1d68a2c64266b25be0544ee9c680bffa
Audrey Leung <audreycml@berkeley.edu>
2017-05-17 16:43:15 +0800

    add portfolio filter

--- js/portfolio.js
+++ js/portfolio.js
@@ -24,3 +24,85 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// ---------------------------------------------- 
+//  Isotope Filter 
+// ---------------------------------------------- 
+(function () {
+	var winDow = $(window);
+	var $container=$('.portfolio-items');
+	var $filter=$('.filter');
+
+	try{
+		$container.imagesLoaded( function(){
+			$container.show();
+			$container.isotope({
+				filter:'*',
+				layoutMode:'masonry',
+				animationOptions:{
+					duration:750,
+					easing:'linear'
+				}
+			});
+		});
+	} catch(err) {
+	}
+
+	winDow.bind('resize', function(){
+		var selector = $filter.find('a.active').attr('data-filter');
+
+		try {
+			$container.isotope({ 
+				filter	: selector,
+				animationOptions: {
+					duration: 750,
+					easing	: 'linear',
+					queue	: false,
+				}
+			});
+		} catch(err) {
+		}
+		return false;
+	});
+
+	$filter.find('a').click(function(){
+		var selector = $(this).attr('data-filter');
+
+		try {
+			$container.isotope({ 
+				filter	: selector,
+				animationOptions: {
+					duration: 750,
+					easing	: 'linear',
+					queue	: false,
+				}
+			});
+		} catch(err) {
+
+		}
+		return false;
+	});
+
+
+	var filterItemA	= $('.filter a');
+
+	filterItemA.on('click', function(){
+		var $this = $(this);
+		if ( !$this.hasClass('active')) {
+			filterItemA.removeClass('active');
+			$this.addClass('active');
+		}
+	});
+}()); 
+
+// -------------------------------------------------------------
+// masonry
+// -------------------------------------------------------------
+
+(function () {
+	var $container = $('.portfolio-items');
+	// initialize
+	$container.masonry({
+	  itemSelector: '.work-grid'
+	});
+}());	
