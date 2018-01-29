ca634283279b3beff5225ac751672ef5d90a283e
Audrey Leung <audreycml@berkeley.edu>
2018-01-28 20:36:27 -0800

    portfolio redesign

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,26 +4,6 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
 
 // ---------------------------------------------- 
 //  Isotope Filter 
@@ -95,14 +75,4 @@ $('.navbar-collapse ul li a').click(function() {
 	});
 }()); 
 
-// // -------------------------------------------------------------
-// // masonry
-// // -------------------------------------------------------------
-
-// (function () {
-// 	var $container = $('.portfolio-items');
-// 	// initialize
-// 	$container.masonry({
-// 	  itemSelector: '.work-grid'
-// 	});
-// }());	
+	
